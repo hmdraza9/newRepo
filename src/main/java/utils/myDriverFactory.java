5688cfc36f4334d72ac61b508dbfefb12656425b
Hamid Raza <hmdraza9@gmail.com>
2025-01-12 00:32:41 +0530

    update to latest selenium

--- src/main/java/utils/myDriverFactory.java
+++ src/main/java/utils/myDriverFactory.java
@@ -1,9 +1,22 @@
 package utils;
 
+import java.io.IOException;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 
 public class myDriverFactory{
 	
@@ -22,16 +35,21 @@ public class myDriverFactory{
 		
 		return driver;
 	}
-	
-	
+
+
 	private static WebDriver BrowserInitSetup() {
-		System.setProperty("webdriver.chrome.driver", "C:/all-driver/chromedriver.exe");
-		System.out.println("Browser initialization");
-		driver = new ChromeDriver();
+        System.out.println("Browser initialization");
+        try {
+			if(driver==null){
+				WebDriverManager.chromedriver().setup();
+				driver = new ChromeDriver();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
 		return driver;
 	}
-	
 }
